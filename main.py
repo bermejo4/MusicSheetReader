e02266d9eb3ac63f287c66c5f2293412a657d836
bermejo4 <minijai72@gmail.com>
2023-01-20 11:30:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pdf2image
 
-path='./Samples/Angry_Birds.pdf'
-images = pdf2image.convert_from_path(path)
 
-for i in range(len(images)):
-    # Save pages as images in the pdf
-    images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+if __name__ == "__main__":
+    path='./Samples/Angry_Birds.pdf'
+    images = pdf2image.convert_from_path(path)
+
+    for i in range(len(images)):
+        # Save pages as images in the pdf
+        images[i].save('page'+ str(i) +'.jpg', 'JPEG')
